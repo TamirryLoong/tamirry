de966898ac0370ba2c14ff1f2350dce6a271a9cb
TamirryLoong <tiddlygit@gmail.com>
2022-07-20 16:39:53 +0800

    Conflict files committed with TiddlyGit-Desktop

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module.exports = {
   semi: true,
   singleQuote: true,
@@ -11,3 +12,18 @@ module.exports = {
   bracketSpacing: true,
   useTabs: false,
 };
+=======
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 160,
+  arrowParens: 'always',
+  jsxBracketSameLine: true,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  useTabs: false,
+};
+>>>>>>> c69d11c (Updated with Git-Sync)
--- workbox-config.js
+++ workbox-config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module.exports = {
   "globDirectory": "public-dist/",
   "maximumFileSizeToCacheInBytes": 1024 * 1024 * 256,
@@ -6,4 +7,14 @@ module.exports = {
   ],
   "swDest": "public-dist/service-worker.js",
   "swSrc": "public/service-worker.js"
+=======
+module.exports = {
+  "globDirectory": "public-dist/",
+  "maximumFileSizeToCacheInBytes": 1024 * 1024 * 256,
+  "globPatterns": [
+    "**/*.{ico,html,js,json,png}"
+  ],
+  "swDest": "public-dist/service-worker.js",
+  "swSrc": "public/service-worker.js"
+>>>>>>> c69d11c (Updated with Git-Sync)
 };
